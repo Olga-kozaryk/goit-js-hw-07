49b0e947d5a2d455e51c0087c3a734e9c58e7360
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-03-15 21:24:01 +0200

    дз на перевірку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    }).join('');
+};
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 console.log(galleryItems);
+
+galleryContainer.addEventListener('click',handleGalleryClick)
+
+function handleGalleryClick(event){
+    event.preventDefault()
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    }
+
+    const modalImg = event.target.dataset.source;
+    const instance = window.basicLightbox.create(`
+    <img src="${modalImg}" 
+    width="800" height="600">`)
+
+    instance.show()
+}
